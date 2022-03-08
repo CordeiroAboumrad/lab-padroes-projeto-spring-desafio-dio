fcfdbfc3c5e81125cf395f781d06aa5f47887f25
CordeiroAboumrad <cordeiro.aboumrad@gmail.com>
2022-03-08 01:48:34 -0300

    Initial commit

---
+++ src/main/java/base/LabPadroesProjetoSpringApplication.java
@@ -0,0 +1,20 @@
+package base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * Projeto Spring Boot feito pelo Spring Initializr
+ *
+ * @author Jean Aboumrad
+ */
+@EnableFeignClients
+@SpringBootApplication
+public class LabPadroesProjetoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabPadroesProjetoSpringApplication.class, args);
+	}
+
+}
---
+++ src/main/java/base/service/ClientService.java
@@ -0,0 +1,12 @@
+package base.service;
+
+import base.model.Cliente;
+
+public interface ClientService {
+    Iterable<Cliente> buscarTodos();
+    Cliente buscarPorId(Long id);
+    void inserir(Cliente cliente);
+    void atualizar(Long id, Cliente cliente);
+    void deletar(Long id);
+
+}
